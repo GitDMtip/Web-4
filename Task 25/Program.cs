1225bcedcb9738c4d83ba0f415f0eda5962a1c42
unknown <bdn2019962@yandex.ru>
2022-08-22 18:30:25 +0400

    Web 4

---
+++ Task 25/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число A: ");
+  int numberA = Convert.ToInt32(Console.ReadLine());
+  Console.WriteLine("Введите число B: ");
+  int numberB = Convert.ToInt32(Console.ReadLine());
+  int exp = Exp(numberA, numberB);
+int Exp(int numberA, int numberB){
+  int result = 1;
+  for(int i=1; i <= numberB; i++)
+  {
+    result = result * numberA;
+  }
+    return result;
+}
+  Console.WriteLine(exp);
---
+++ Task 29/Program.cs
@@ -0,0 +1,14 @@
+﻿    Console.WriteLine("Введите количество элементов массива");
+    int el = int.Parse(Console.ReadLine());
+    int[] arr = new int[el];
+    Console.WriteLine("Введите массив:");
+    for(int i = 0; i < arr.Length; i++)
+    {
+       Console.Write($"Введите элемент массива под индексом {i}:\t ");
+        arr[i] = int.Parse(Console.ReadLine());
+    }  
+    Console.WriteLine("Наш массив");
+    for (int i = 0;  i < arr.Length; i++)
+    {
+    Console.WriteLine(arr[i]);
+    }
